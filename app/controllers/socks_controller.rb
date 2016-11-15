14dc5458804a221495e0b66c931fba28207fad1d
Brent Watson <brentmwatson@gmail.com>
2016-11-15 18:46:07 -0500

    'stlye' changed to 'style' and controller for sock index and 'show' started

--- app/controllers/socks_controller.rb
+++ app/controllers/socks_controller.rb
@@ -1,2 +1,28 @@
 class SocksController < ApplicationController
+  def index #has template renders view
+    @socks = Sock.all
+    #split and stuff
+    render :json @socks
+  end
+
+  def show #has template, renders view
+    @socks = Sock.find(params :id)
+    render :json @socks
+  end
+
+  # def new #has template, displays form
+  # end
+  #
+  # def create #saves and redirects, saves new record
+  # end
+  #
+  # def edit #has template, diplays form of exsisting record
+  # end
+  #
+  # def update #saves and redirects, saves changes
+  # end
+  #
+  # def destroy  #destorys and redirects
+  # end
+
 end
