e3de1f687ed09cee75082a98633c25757f23035e
jkrouse13 <jon.krouse@gmail.com>
2016-11-15 13:54:56 -0500

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+ root 'welcome#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
