73d3690eaf7ab3622bd328dd8c33a96fdfe1d000
Tom McClelland <tom@mcclelland.me>
2016-11-15 14:43:50 -0500

    added mockup routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     get 'welcome/index'
-
+    get '/sockdetail' => 'welcome#sockDetail'
+    get '/cart' => 'welcome#cart'
+    get '/checkout' => 'welcome#checkout'
      root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
