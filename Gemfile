787c3d60957aaa966ec92f4bd9fa5dad7ddaad8e
jkrouse13 <jon.krouse@gmail.com>
2016-11-16 14:57:31 -0500

    just beginning to pull down information from google

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
+gem "figaro"
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -36,6 +37,7 @@ gem "refile", require: "refile/rails", github: 'refile/refile'
 gem "refile-mini_magick"
 gem 'refile-postgres', '~> 1.4'
 gem 'rails_admin', '~> 1.0'
+gem 'google_drive'
 
 
 group :development, :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "google_drive"
+session = GoogleDrive::Session.from_config("config.json")
+# session = GoogleDrive::Session.from_service_account_key(StringIO.new(ENV['google_secret']))
+# Sock.create(
+#   style: Style.find_or_initialize_by(name: "crew"),
+#   color: Color.find_or_initialize_by(name: "grey"),
+#   name: "The Sock Monkey",
+#   price: 2700
+# )
+ws = session.spreadsheet_by_key("1m_x4ZtQzTHYldutf_i90uXSfKfEjFXSPvyV_L58nDJ0").worksheets[0]
 
-Sock.create(
-  style: Style.find_or_initialize_by(name: "crew"),
-  color: Color.find_or_initialize_by(name: "grey"),
-  name: "The Sock Monkey",
-  price: 2700
-)
+
+(1..ws.num_rows).each do |row|
+  (1..ws.num_cols).each do |col|
+    p ws[row, col]
+  end
+end
