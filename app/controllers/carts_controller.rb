c4e20685fec8dca81f558b3c0a8ce9680d6dcac0
Brent Watson <brentmwatson@gmail.com>
2016-11-16 15:49:59 -0500

    start of models for cart, and line_item

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,25 @@
 class CartsController < ApplicationController
+  # def index #has template renders view
+  # end
+
+  def show #has template, renders view
+    @cart= cart.find_by(params[:token])
+    @cart.line_items
+    render json: @cart
+  end
+
+  # def new #has template, displays form
+  # end
+  #
+  # def create #saves and redirects, saves new record
+  # end
+  #
+  # def edit #has template, diplays form of exsisting record
+  # end
+  #
+  # def update #saves and redirects, saves changes
+  # end
+  #
+  # def destroy  #destorys and redirects
+  # end
 end
