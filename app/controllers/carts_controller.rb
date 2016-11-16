2d5ee431bffdf31a221460187285d08cac62907a
Troy Denney <tvdenney@gmail.com>
2016-11-16 15:32:28 -0500

    cart updates

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,7 @@
 class CartsController < ApplicationController
+    def index #has template renders view
+     @carts = Cart.all
+     #split and stuff
+     render json: @carts
+   end
 end
--- webpack/components/App.js
+++ webpack/components/App.js
@@ -12,7 +12,13 @@ class App extends React.Component {
     render() {
         return <div>
             <Header />
-            {this.props.children}
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-12">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
     }
