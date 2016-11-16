8d2680b15fb71f5a2bbd49e8d7cec6a2e354d543
jkrouse13 <jon.krouse@gmail.com>
2016-11-16 18:37:58 -0500

    added migrations to handle images properly, seed hangs because of images now

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,28 +15,29 @@ session = GoogleDrive::Session.from_config("config.json")
 #   price: 2700
 # )
 blend = session.spreadsheet_by_key("1m_x4ZtQzTHYldutf_i90uXSfKfEjFXSPvyV_L58nDJ0").worksheets[0]
-(1..blend.num_rows).each do |row|
+(2..blend.num_rows).each do |row|
   blend[row,5].split(",").collect(&:strip).each do |abr|
     Size.create!(
-    arrb: abr.match(/\[A-Z]/i)[0],
-    stock_quantitiy: abr.match(/\d+/).to_i,
+    arrb: abr.match(/[A-Z]/i)[0],
+    stock_quantitiy: abr.match(/\d+/)[0].to_i,
     sock: Sock.create!(
       name: blend[row, 1],
       color: Color.where(name: blend[row, 2]).first_or_create,
       style: Style.where(name: blend[row, 3]).first_or_create,
       price: blend[row, 6].gsub(/\D+/, "").to_i * 100,
       material: blend[row, 8],
-      category: Category.where(name: "Blended").first_or_create
+      category: Category.where(name: "Blended").first_or_create,
+      remote_image_url: blend[row, 7]
       )
     )
   end
 end
 llama = session.spreadsheet_by_key("1m_x4ZtQzTHYldutf_i90uXSfKfEjFXSPvyV_L58nDJ0").worksheets[1]
-(1..llama.num_rows).each do |row|
+(2..llama.num_rows).each do |row|
   blend[row,5].split(",").collect(&:strip).each do |abr|
     Size.create!(
-    arrb: abr.match(/\[A-Z]/i)[0],
-    stock_quantitiy: abr.match(/\d+/).to_i,
+    arrb: abr.match(/[A-Z]/i)[0],
+    stock_quantitiy: abr.match(/\d+/)[0].to_i,
     sock: Sock.create!(
       name: llama[row, 1],
       color: Color.where(name: llama[row, 2]).first_or_create,
@@ -44,47 +45,45 @@ llama = session.spreadsheet_by_key("1m_x4ZtQzTHYldutf_i90uXSfKfEjFXSPvyV_L58nDJ0
       price: llama[row, 6].gsub(/\D+/, "").to_i * 100,
       material: llama[row, 8],
       category: Category.where(name: "Llama").first_or_create,
-      description: llama[row,9]
+      description: llama[row, 9],
+      remote_image_url: llama[row, 7]
       )
     )
   end
 end
 alpaca = session.spreadsheet_by_key("1m_x4ZtQzTHYldutf_i90uXSfKfEjFXSPvyV_L58nDJ0").worksheets[0]
-(1..alpaca.num_rows).each do |row|
+(2..alpaca.num_rows).each do |row|
   blend[row,5].split(",").collect(&:strip).each do |abr|
     Size.create!(
-    arrb: abr.match(/\[A-Z]/i)[0],
-    stock_quantitiy: abr.match(/\d+/).to_i,
+    arrb: abr.match(/[A-Z]/i)[0],
+    stock_quantitiy: abr.match(/\d+/)[0].to_i,
     sock: Sock.create!(
       name: alpaca[row, 1],
       color: Color.where(name: alpaca[row, 2]).first_or_create,
       style: Style.where(name: alpaca[row, 3]).first_or_create,
       price: blend[row, 6].gsub(/\D+/, "").to_i * 100,
       material: alpaca[row, 8],
-      category: Category.where(name: "Alpaca").first_or_create
+      category: Category.where(name: "Alpaca").first_or_create,
+      remote_image_url: alpaca[row, 7]
       )
     )
   end
 end
 wool = session.spreadsheet_by_key("1m_x4ZtQzTHYldutf_i90uXSfKfEjFXSPvyV_L58nDJ0").worksheets[0]
-(1..blend.num_rows).each do |row|
+(2..blend.num_rows).each do |row|
   wool[row,5].split(",").collect(&:strip).each do |abr|
     Size.create!(
-    arrb: abr.match(/\[A-Z]/i)[0],
-    stock_quantitiy: abr.match(/\d+/).to_i,
+    arrb: abr.match(/[A-Z]/i)[0],
+    stock_quantitiy: abr.match(/\d+/)[0].to_i,
     sock: Sock.create!(
       name: wool[row, 1],
       color: Color.where(name: wool[row, 2]).first_or_create,
       style: Style.where(name: wool[row, 3]).first_or_create,
       price: wool[row, 6].gsub(/\D+/, "").to_i * 100,
       material: wool[row, 8],
-      category: Category.where(name: "Wool").first_or_create
+      category: Category.where(name: "Wool").first_or_create,
+      remote_image_url: wool[row, 7]
       )
     )
   end
 end
-# (1..ws.num_rows).each do |row|
-#   (1..ws.num_cols).each do |col|
-#     p ws[row, col]
-#   end
-# end
