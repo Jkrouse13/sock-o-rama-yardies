6f440592c21eea53e75a37ce33e4ee404a7a90a8
jkrouse13 <jon.krouse@gmail.com>
2016-11-17 14:39:27 -0500

    Changed seed to be many sizes for one sock instead of one to one.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require "google_drive"
 session = GoogleDrive::Session.from_config("config.json")
-# session = GoogleDrive::Session.from_service_account_key(StringIO.new(ENV['google_secret']))
-# Sock.create(
-#   style: Style.find_or_initialize_by(name: "crew"),
-#   color: Color.find_or_initialize_by(name: "grey"),
-#   name: "The Sock Monkey",
-#   price: 2700
-# )
+
 blend = session.spreadsheet_by_key("1m_x4ZtQzTHYldutf_i90uXSfKfEjFXSPvyV_L58nDJ0").worksheets[0]
 (2..blend.num_rows).each do |row|
   blend[row,5].split(",").collect(&:strip).each do |abr|
