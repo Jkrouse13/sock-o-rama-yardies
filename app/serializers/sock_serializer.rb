37705f5ad4af96a3ce80b3bfdbb205cd178f853e
jkrouse13 <jon.krouse@gmail.com>
2016-11-17 15:57:35 -0500

    images should now hopefully work

--- app/serializers/sock_serializer.rb
+++ app/serializers/sock_serializer.rb
@@ -2,9 +2,13 @@ class SockSerializer < ActiveModel::Serializer
   # attributes :id, :name, :price, :description, :image, :featured, :material
   # :image was breaking the response backt to the front end
   # TODO: Fix once images are working.
-  attributes :id, :name, :price, :description, :featured, :material
+  attributes :id, :name, :price, :description, :featured, :material, :image
   has_one :color
   has_one :style
   has_one :category
   has_many :sizes
+
+  def image
+   Refile.attachment_url(object, :image, :fit, 300, 300, format: "jpg")
+  end
 end
