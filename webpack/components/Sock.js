a0d44dc9038607d997f02a17a12bff84d009004c
Tom McClelland <tom@mcclelland.me>
2016-11-17 12:57:33 -0500

    intial adding to cart

--- webpack/components/Sock.js
+++ webpack/components/Sock.js
@@ -1,17 +1,164 @@
 import React from 'react'
+import { sharedState, attachSharedState, detachSharedState } from 'react-helpers/dist/sharedState'
+import classAutoBind from 'react-helpers/dist/classAutoBind'
+import accounting from 'accounting-js'
+import Modal from 'react-modal'
 
+class Sock extends React.Component {
+    constructor(props) {
+        super(props)
+        classAutoBind(this)
+        this.state = {
+            // sharedState: sharedState(),
+            modalIsOpen: false,
+            token: '',
+            sock_id: '',
+            quantity: 0,
+            size_id: ''
+        }
+        // this.state = sharedState()
+    }
 
-const Sock = (props) => <div className="col-sm-6 col-md-4">
-      <div className="thumbnail">
-        <img src={props.sock.image} alt="..."   />
-        <div className="caption">
-          <h3>Thumbnail label</h3>
-          <p>{props.sock.name}</p>
-          <p><a href="#" className="btn btn-primary" role="button">Button</a></p>
-        </div>
-      </div>
-    </div>
+    componentDidMount() {
+        attachSharedState(this, (state) => this.setState({sharedState: state}))
+        // attachSharedState(this)
+    }
 
+    componentWillUnmount() {
+        detachSharedState(this)
+    }
 
+    openModal() {
+      this.setState({modalIsOpen: true})
+    }
+
+    afterOpenModal() {
+      // references are now sync'd and can be accessed.
+      // this.refs.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+      this.setState({modalIsOpen: false})
+    }
+
+    addToCart() {
+        fetch('/add_cart_item', {
+            method: 'POST',
+            // body: JSON.stringify({
+            //     token: this.state.token,
+            //     sock_id: this.state.sock_id,
+            //     quantity: this.state.quantity,
+            //     size_id: this.state.size_id,
+            // })
+            body: JSON.stringify({
+                token: this.state.token,
+                sock_id: this.state.sock_id,
+                quantity: this.state.quantity,
+                size_id: this.state.size_id,
+            })
+        })
+        .then(response => response.json())
+        .then(this.handleAddToCart())
+
+        // var response = {
+        //     success: true
+        // }
+
+        // this.handleAddToCart(response)
+    }
+
+    handleAddToCart(response) {
+        this.closeModal()
+        console.log(response)
+        var items = sharedState().itemsInCart
+        items++
+        console.log(items)
+
+        sharedState({
+            itemsInCart: items
+        })
+    }
+
+    render() {
+        return <div className="col-sm-6 col-md-4">
+
+              <div className="thumbnail">
+                {/* <img src={this.props.sock.image} alt="..."   /> */}
+                {/* // TODO: Remove hardcoded image */}
+                 <img className="img-responsive" src="http://www.llamafibercoop.com/Socks%20no%20pricing.jpg" width="400" alt="sock image" />
+                <div className="caption">
+                  <h3>{this.props.sock.name}</h3>
+                  <p>{this.props.sock.description}</p>
+                  <p>{accounting.format(this.props.sock.price/100)}</p>
+                  <button className="btn btn-primary" onClick={this.openModal}>View Details</button>
+                 <Modal
+                   isOpen={this.state.modalIsOpen}
+                   onAfterOpen={this.afterOpenModal}
+                   onRequestClose={this.closeModal}
+                   style={customStyles}
+                   contentLabel="Example Modal">
+                   <div className="modal-dialog modal-lg" role="document">
+                     <div className="modal-content">
+                           <div className="modal-header">
+                             <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" onClick={this.closeModal}>&times;</span></button>
+                             <h4 className="modal-title" id="gridSystemModalLabel">{this.props.sock.name}</h4>
+                           </div>
+                           <div className="modal-body">
+                             <div className="row">
+                               <div className="col-md-6">
+                                 <img className="img-responsive" src="http://www.llamafibercoop.com/Socks%20no%20pricing.jpg" width="400" alt="sock image" />
+                                 <h2>Description</h2>
+                                 <p>{this.props.sock.description}</p>
+                                 <h2>Materials</h2>
+                                 <p>Llama</p>
+                               </div>
+                               <div className="col-md-6">
+                                 <h3>Price: {this.props.sock.price}</h3>
+                                   <div className="form-group">
+                                         <label htmlFor="size">Size</label>
+                                         <select id ="size" className="form-control">
+                                           <option value="sm">Small</option>
+                                           <option value="md">Medium</option>
+                                           <option value="lg">Large</option>
+                                         </select>
+                                   </div>
+                                   <div className="form-group">
+                                         <label htmlFor="color">Color</label>
+                                         <select className="form-control" id="color">
+                                           <option value="charcoal">Charcoal</option>
+                                         </select>
+                                   </div>
+                                   <div className="form-group">
+                                       <label htmlFor="style">Style</label>
+                                         <select id="style" className="form-control">
+                                           <option value="knee">Knee</option>
+                                         </select>
+                                   </div>
+                               </div>
+                             </div>
+                           </div>
+                           <div className="modal-footer">
+                             <button type="button" className="btn btn-default" data-dismiss="modal" onClick={this.closeModal}>Close</button>
+                             <button type="button" className="btn btn-primary" onClick={this.addToCart}>Add to Cart</button>
+                           </div>
+                         </div>
+                   </div>
+                 </Modal>
+                </div>
+              </div>
+            </div>
+    }
+}
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 export default Sock
