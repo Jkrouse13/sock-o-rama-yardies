a78631b94e0a73b52f2fca267db0e050fb4454af
jkrouse13 <jon.krouse@gmail.com>
2016-11-18 11:01:35 -0500

    changed serializer to send more info, and now render the cart for cart show.

--- app/serializers/cart_serializer.rb
+++ app/serializers/cart_serializer.rb
@@ -1,4 +1,5 @@
 class CartSerializer < ActiveModel::Serializer
-  attributes :id, :token, :street, :email, :first_name, :last_name, :complete, :city, :state, :zip, :country, :sub_total, :tax, :shipping, :grand_total
+  attributes :id, :token, :street, :email, :first_name, :last_name, :complete, :city, :state, :zip, :country, :sub_total, :tax, :shipping, :grand_total, :line_items
   has_many :line_items
+  
 end
--- app/serializers/line_item_serializer.rb
+++ app/serializers/line_item_serializer.rb
@@ -1,8 +1,7 @@
 class LineItemSerializer < ActiveModel::Serializer
-  attributes :id, :item_quantity, :cost
+  attributes :id, :item_quantity, :cost, :sock
   has_one :cart
   has_one :size
-
-
+  has_one :sock
 
 end
